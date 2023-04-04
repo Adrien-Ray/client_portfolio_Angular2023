eefae02f438fe15f8f657039c0ef6a7a17710e3f
Adrien-Ray <a.raymond@codeur.online>
2023-04-04 12:00:53 +0200

    landing supr feature http request

--- src/app/page-landing/page-landing.component.ts
+++ src/app/page-landing/page-landing.component.ts
@@ -3,23 +3,14 @@ import { Project } from '../models/project.model';
 import { ProjectsService } from "../services/projects.service";
 
 @Component({
-  selector: 'app-page-landing',
-  templateUrl: './page-landing.component.html',
-  styleUrls: ['./page-landing.component.scss']
+    selector: 'app-page-landing',
+    templateUrl: './page-landing.component.html',
+    styleUrls: ['./page-landing.component.scss']
 })
 export class PageLandingComponent implements OnInit{
-  projects!: Project[];
-  projectTest: any = [];
-  constructor(private projectService: ProjectsService){}
-  ngOnInit(): void {
-    this.projects = this.projectService.getAllProjects();
-    this.projectTest = this.fetchUsers();
-    /* this.projectsTest = this.projectService.getUsers(); */
-    console.log(this.projectTest);
-    
-  }
-
-  fetchUsers() {
-    return this.projectService.getUsers();
-  }
+    projects!: Project[];
+    constructor(private projectService: ProjectsService){}
+    ngOnInit(): void {
+        this.projects = this.projectService.getAllProjects();
+    }
 }
