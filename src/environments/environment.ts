239c619eaad7f2433b70b0d2134fd90c3df92108
Adrien-Ray <a.raymond@codeur.online>
2023-04-18 13:55:08 +0200

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,13 @@
 export const environment = {
   production: false,
+  apiEndPoint:"http://localhost/perso/domains-portfolio/api/",
+  uploadFolder: "http://localhost/perso/domains-portfolio/assets/img/upload/"
+};
+
+// code for environment.prod.ts (safe)
+
+/* export const environment = {
+  production: true,
   apiEndPoint:"https://portfolio.accesdenied.net/api/",
   uploadFolder: "https://portfolio.accesdenied.net/assets/img/upload/"
-};
+}; */
