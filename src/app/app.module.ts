0b10ba2bcc188c1eb2fae086bbc6b481ada1eb85
Adrien-Ray <a.raymond@codeur.online>
2023-04-19 09:51:04 +0200

    reactiv form and func onClick

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -60,6 +60,7 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatTreeModule } from '@angular/material/tree'; */
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -121,6 +122,12 @@ import { MatTreeModule } from '@angular/material/tree'; */
     MatToolbarModule,
     MatTooltipModule,
     MatTreeModule, */
+
+
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [
     {provide: LocationStrategy, useClass: HashLocationStrategy}
--- src/app/page-contact/page-contact.component.ts
+++ src/app/page-contact/page-contact.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-page-contact',
   templateUrl: './page-contact.component.html',
   styleUrls: ['./page-contact.component.scss']
 })
-export class PageContactComponent {
-
+export class PageContactComponent implements OnInit {
+  form!: FormGroup;
+  constructor(private formBuilder : FormBuilder){}
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+        mail: [null],
+        message: [null]
+    });
+  }
+  onSubmitForm() {
+    console.log(this.form.value);
+}
 }
